f84d577677cff520d7c05021212dcb43fc83d415
Cristin <cristin@optimum-web.com>
2019-02-18 09:51:23 +0200

    fix

--- 5.7/backend/config/app.php
+++ 5.7/backend/config/app.php
@@ -162,6 +162,9 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        /* Vault */
+        //Tokenly\Vault\VaultServiceProvider::class,
+
         /*
          * Package Service Providers...
          */
